3256a34e1aef7d571944f750e23c59c162352847
Nathen Amestoy <nate1347@gmail.com>
2022-06-16 17:28:23 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("fetch-activity").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById("activity").textContent = data.activity
+      })
+  })
